5fdb2bdcfc15da42d690c70403e1cac6439e0143
rachel <rachelricedev@gmail.com>
2024-09-23 20:35:01 -0700

    adding middleware to server

--- server.js
+++ server.js
@@ -9,4 +9,17 @@ const port = process.env.PORT || 4000
 
 // Connect to MongoDB
 
-mongoose.connect()
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.fkj2i.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`)
+    .then(() => console.log('Mongo DB Connected'))
+    .catch(err => console.log(err))
+
+// Middleware (ahead of any data processing)
+
+//enable CORS
+app.use(cors())
+
+//serve static files
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
