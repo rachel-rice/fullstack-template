0489e2efe547cec3062aa88d69b665d3520fe3eb
rachel <rachelricedev@gmail.com>
2025-02-08 11:46:47 -0800

    fixed form actions to match the route

--- routes/item.js
+++ routes/item.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const itemController = require("../controllers/item");
 
-// Define your routes and use the controller functions
+// Define routes and use the controller functions
 router.get('/', itemController.getItems);
 router.post('/add', itemController.addItem);
 router.post('/update/:id', itemController.updateItem);
